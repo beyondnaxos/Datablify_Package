e9dbc25181bc80348db6c070f70fc15bf5702656
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-31 17:02:21 +0200

    Styling Table comp

--- src/components/Datablify/Datablify.jsx
+++ src/components/Datablify/Datablify.jsx
@@ -1,49 +1,50 @@
-import Styles from "./Datablify.module.css"
+import styles from "./Datablify.module.css"
 
 export const Datablify = ({ data }) => {
+
   return (
-    <section className={Styles.tableCompContainer}>
-      <div className={Styles.viewAndSearch}>
-        <div className={Styles.limitViewContainer}>
-          <span>Show</span>
+    <section className={styles.tableCompContainer}>
+      <div className={styles.viewAndSearch}>
+        <div className={styles.limitViewContainer}>
+          <span className={styles.spaninfo}>Show</span>
           <select>
             <option value="10">10</option>
             <option value="25">25</option>
             <option value="50">50</option>
             <option value="100">100</option>
           </select>
-          <span>entries</span>
+          <span className={styles.spaninfo} >entries</span>
         </div>
         <div>
-          <input type="text" placeholder="Search..." />
+          <input type="text" className={styles.searchInput}placeholder="Search..." />
         </div>
       </div>
-      <table>
+      <table className={styles.tableContainer}>
         <thead>
           <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Start Date</th>
-            <th>Department</th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip Code</th>
+            <th className={styles.title}>First Name</th>
+            <th className={styles.title}>Last Name</th>
+            <th className={styles.title}>Start Date</th>
+            <th className={styles.title}>Department</th>
+            <th className={styles.title}>Date of Birth</th>
+            <th className={styles.title}>Street</th>
+            <th className={styles.title}>City</th>
+            <th className={styles.title}>State</th>
+            <th className={styles.title}>Zip Code</th>
           </tr>
         </thead>
         <tbody>
           {data?.map((row, index) => (
-            <tr key={index}>
-              <td>{row.firstName}</td>
-              <td>{row.lastName}</td>
-              <td>{row.startDate}</td>
-              <td>{row.department}</td>
-              <td>{row.dateOfBirth}</td>
-              <td>{row.street}</td>
-              <td>{row.city}</td>
-              <td>{row.state}</td>
-              <td>{row.zipCode}</td>
+            <tr className={styles.tableRow} key={index}>
+              <td className={styles.rowData}>{row.firstName}</td>
+              <td className={styles.rowData}>{row.lastName}</td>
+              <td className={styles.rowData}>{row.startDate}</td>
+              <td className={styles.rowData}>{row.department}</td>
+              <td className={styles.rowData}>{row.dateOfBirth}</td>
+              <td className={styles.rowData}>{row.street}</td>
+              <td className={styles.rowData}>{row.city}</td>
+              <td className={styles.rowData}>{row.state}</td>
+              <td className={styles.rowData}>{row.zipCode}</td>
             </tr>
           ))}
         </tbody>
