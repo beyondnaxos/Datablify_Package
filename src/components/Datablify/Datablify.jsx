c8a12333f62f5a8b0908193b52aaec505583a82e
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-31 15:20:29 +0200

    Adding options and searchbar

--- src/components/Datablify/Datablify.jsx
+++ src/components/Datablify/Datablify.jsx
@@ -1,3 +1,53 @@
-export const Datablify = () => {
-  return (<>HelloWorld</>)
+import Styles from "./Datablify.module.css"
+
+export const Datablify = ({ data }) => {
+  return (
+    <section className={Styles.tableCompContainer}>
+      <div className={Styles.viewAndSearch}>
+        <div className={Styles.limitViewContainer}>
+          <span>Show</span>
+          <select>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+          <span>entries</span>
+        </div>
+        <div>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Start Date</th>
+            <th>Department</th>
+            <th>Date of Birth</th>
+            <th>Street</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((row, index) => (
+            <tr key={index}>
+              <td>{row.firstName}</td>
+              <td>{row.lastName}</td>
+              <td>{row.startDate}</td>
+              <td>{row.department}</td>
+              <td>{row.dateOfBirth}</td>
+              <td>{row.street}</td>
+              <td>{row.city}</td>
+              <td>{row.state}</td>
+              <td>{row.zipCode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  )
 }
--- src/stories/Datablify.stories.jsx
+++ src/stories/Datablify.stories.jsx
@@ -6,4 +6,26 @@ import { Datablify } from '../components/Datablify'
 
 const stories = storiesOf('Datablify', module)
 
-stories.add('Default', () => <Datablify />)
+stories.add('Default', () => {
+    const data = [
+        {
+          firstName: "Patrick",
+          lastName: "Bouboulina",
+          startDate: "2022-01-01",
+          department: "Info",
+          dateOfBirth: "1992-01-01",
+          street: "189 rue de la Ventouse",
+          city: "Bouba-Ville",
+          state: "France",
+          zipCode: "45650",
+        },
+        // Ajoutez plus de lignes de données ici...
+      ];
+    
+      return (
+        <div>
+          <h1>Exemple de tableau</h1>
+          <Datablify data={data} />
+        </div>
+      );
+})
