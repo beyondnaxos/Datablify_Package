b678fd7d7c9b3c958f3dcd6455fa90ce784402e9
BeyondNaxos <adopteuntrip@gmail.com>
2023-04-01 12:51:00 +0200

    CopyToClipboard OK

--- src/components/Datablify/Datablify.jsx
+++ src/components/Datablify/Datablify.jsx
@@ -2,45 +2,75 @@ import styles from './Datablify.module.css'
 
 export const Datablify = ({ data, categories }) => {
 
+  const isValidData = categories.length === Object.keys(data[0]).length
+
+  const copyToClipboard = (e) => {
+    const text = e.target.innerText
+    navigator.clipboard.writeText(text)
+    e.target.innerText = 'Copied'
+    setTimeout(() => {
+      e.target.innerText = text
+    }, 750)
+  }
+
   return (
     <section className={styles.tableCompContainer}>
-      <div className={styles.viewAndSearch}>
-        <div className={styles.limitViewContainer}>
-          <span className={styles.spaninfo}>Show</span>
-          <select>
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </select>
-          <span className={styles.spaninfo}>entries</span>
-        </div>
-        <div>
-          <input
-            type="text"
-            className={styles.searchInput}
-            placeholder="Search..."
-          />
-        </div>
-      </div>
-      <table className={styles.tableContainer}>
-        <thead>
-          <tr>
-            {categories.map((category) => (
-              <th className={styles.title}>{category}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((row, index) => (
-            <tr className={styles.tableRow} key={index}>
-              {Object.entries(row).map(([key, value]) => (
-                <td className={styles.rowData}>{value}</td>
+      {isValidData ? (
+        <>
+          <div className={styles.viewAndSearch}>
+            <div className={styles.limitViewContainer}>
+              <span className={styles.spaninfo}>Show</span>
+              <select>
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+              </select>
+              <span className={styles.spaninfo}>entries</span>
+            </div>
+            <div>
+              <input
+                type="text"
+                className={styles.searchInput}
+                placeholder="Search..."
+              />
+            </div>
+          </div>
+          <table className={styles.tableContainer}>
+            <thead>
+              <tr>
+                {categories.map((category) => (
+                  <th className={styles.title}>{category}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((row, index) => (
+                <tr className={styles.tableRow} key={index}>
+                  {Object.entries(row).map(([key, value]) => (
+                    <td className={styles.rowData} onClick={(e) => copyToClipboard(e)}>{value}</td>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <div className={styles.errorContainer}>
+          <h1 className={styles.errorTitle}>Error</h1>
+          <p className={styles.errorText}>
+            Categories and datas are not corresponding
+          </p>
+          <p className={styles.errorCompare}>
+            you have <span className={styles.counter}>{categories.length}</span>{' '}
+            categories and{' '}
+            <span className={styles.counter}>
+              {Object.keys(data[0]).length}
+            </span>{' '}
+            value(s) per row{' '}
+          </p>
+        </div>
+      )}
     </section>
   )
 }
