7f37e5f022b5d5e8c96a344a76b7ccb5459dfbd2
BeyondNaxos <adopteuntrip@gmail.com>
2023-04-08 11:37:40 +0200

    clean pagination OK

--- src/components/Datablify/Pagination.jsx
+++ src/components/Datablify/Pagination.jsx
@@ -1,29 +1,23 @@
 import React from 'react'
 import styles from './Datablify.module.css'
 
-const Pagination = ({nPages, currentPage, setCurrentPage}) => {
+const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
   const pageNumbers = [...Array(nPages + 1).keys()].slice(1)
 
   const nextPage = () => {
-    if(currentPage !== nPages) 
-        console.log(currentPage)
-        setCurrentPage(currentPage + 1)
-}
-const prevPage = () => {
-    if(currentPage !== 1) 
-        console.log(currentPage)
-        setCurrentPage(currentPage - 1)
-}
+    if (currentPage !== nPages) console.log(currentPage)
+    setCurrentPage(currentPage + 1)
+  }
+  
+  const prevPage = () => {
+    if (currentPage !== 1) console.log(currentPage)
+    setCurrentPage(currentPage - 1)
+  }
 
   return (
     <div className={styles.pagination}>
-      <button
-        className={styles.paginationButton}
-        onClick={prevPage}
-      >
-        <span className={styles.paginationButtonIcon}>
-         {"<"}
-        </span>
+      <button className={styles.paginationButton} onClick={prevPage}>
+        <span className={styles.paginationButtonIcon}>{'<'}</span>
       </button>
       {pageNumbers.map((number) => (
         <button
@@ -34,13 +28,8 @@ const prevPage = () => {
           {number}
         </button>
       ))}
-      <button
-        className={styles.paginationButton}
-        onClick={nextPage}
-      >
-        <span className={styles.paginationButtonIcon}>
-        {">"}
-        </span>
+      <button className={styles.paginationButton} onClick={nextPage}>
+        <span className={styles.paginationButtonIcon}>{'>'}</span>
       </button>
     </div>
   )
