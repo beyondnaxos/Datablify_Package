a220ff9dd991ec11eb24b778a7b8200ceb2a041a
BeyondNaxos <adopteuntrip@gmail.com>
2023-04-14 12:30:14 +0200

    StoryBook 7.0 major changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
   "core": {
     "builder": 'webpack5',
   },
-  "framework": "@storybook/react"
-}
+};
